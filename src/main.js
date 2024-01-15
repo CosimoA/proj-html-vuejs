0d13e93986ea0bab1f2321e576143895e0b63181
CosimoA <cosimo.archip@gmail.com>
2024-01-15 18:17:33 +0100

    CONFIG: setup base scaffolding

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
-import './style.css'
+import '@fortawesome/fontawesome-free/css/all.css';
 import App from './App.vue'
+import { router } from './router';
 
+createApp(App).use(router).mount('#app');
 createApp(App).mount('#app')
---
+++ src/router.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import Home from './pages/Home.vue';
+import About from './pages/About.vue';
+import Contacts from './pages/Contacts.vue';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        }, {
+            path: '/About',
+            name: 'About',
+            component: About
+        }, {
+            path: '/Contacts',
+            name: 'Contacts',
+            component: Contacts
+        },
+    ]
+});
+export { router };
