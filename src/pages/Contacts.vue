33d78d2bc2ea0a96963b1d970b30e28bf8e6adfa
CosimoA <cosimo.archip@gmail.com>
2024-01-16 16:33:22 +0100

    FIX: restore real name to Contact.vue

---
+++ src/pages/Contacts.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: "Contacts"
+} 
+</script> 
+
+<template>
+    <main>
+        <section>
+            <h1 class="mt-5 mb-3">Contacts</h1>
+        </section>
+    </main>
+</template> 
+
+<style scoped></style>
